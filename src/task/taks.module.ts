69973c43fd5aca4735656f8e88fc71a6ef704884
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-17 15:44:33 -0300

    feat: Add MongooseModule to TaksModule for MongoDB integration

--- src/task/taks.module.ts
+++ src/task/taks.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongo } from 'mongoose';
+import { Task, TaskSchema } from '../schemas/task.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+  ],
+})
 export class TaksModule {}
