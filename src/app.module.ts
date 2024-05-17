86a7111f578f554e1144f72f9fcb0c21178faaf4
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-17 14:51:15 -0300

    feat: add service, module, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TaksModule } from './task/taks.module';
+import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TaksModule],
+  controllers: [, TaskController],
+  providers: [, TaskService],
 })
 export class AppModule {}
